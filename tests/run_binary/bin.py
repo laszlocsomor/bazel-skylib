10fdaf471999285142059b68f973072ff64f6879
Laszlo Csomor <laszlocsomor@google.com>
2019-05-09 13:10:10 +0200

    run_binary: runs an executable as an action
    This rule is an alternative for genrule(): it can
    run a binary with the desired arguments,
    environment, inputs, and outputs, as a single
    build action, without shelling out to Bash.
    
    Fixes https://github.com/bazelbuild/bazel-skylib/issues/149

---
+++ tests/run_binary/bin.py
@@ -0,0 +1,30 @@
+# Copyright 2019 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from bazel_tools.tools.python.runfiles import runfiles
+import os
+import sys
+
+r = runfiles.Create()
+if not r:
+  raise Exception("Could not initialize runfiles")
+with open(sys.argv[1], "wt") as dst:
+  # Separate lines with "|" instead of newline, to avoid problems with different
+  # line endings on Linux vs. Windows.
+  dst.write("ENV2=(%s)" % os.environ.get("ENV2"))
+  dst.write("|")
+  for i in range(2, 6):
+    dst.write(sys.argv[i])
+    dst.write("|")
+  with open(r.Rlocation("bazel_skylib/tests/run_binary/data.txt"), "rt") as src:
+    dst.write("|".join(line.strip() for line in src.readlines()))
